7eba63b29aecca512bbbab0a67c5d4f23b335d68
DEVKomron <komronmamataliyev@gmail.com>
2025-03-12 15:45:10 +0500

    yakun

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -36,7 +36,7 @@ async create(createUserDto: CreateUserDto) {
   });
 
 
-  const activationUrl = `3.78.122.117/user/activate/${activationLink}`;
+  const activationUrl = `3.78.122.117:3030/user/activate/${activationLink}`;
   await this.mailService.sendActivationEmail(user.email, activationUrl);
 
   return {
